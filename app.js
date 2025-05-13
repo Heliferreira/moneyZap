fe04ceb7c7b2f8217054ea693d6c1f744b76bf9a
Heliferreira <heliveltonsouza@outlook.com>
2025-05-13 18:49:58 -0300

    Corrige porta para 10000 no Render

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ app.get('/backup', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Bot rodando na porta ${PORT}`);
 });
