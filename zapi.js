cae3466c59c0f0ac00c6dee686c920836ce3b353
Heliferreira <heliveltonsouza@outlook.com>
2025-05-14 10:07:14 -0300

    Corrige API_URL com instância e token reais

--- zapi.js
+++ zapi.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const INSTANCIA_ID = '3E126FC63F55D002CB47AAEF140028B5';
 const TOKEN = '2041E2CA4AF17D4509230A8D';
 
-const API_URL = `https://api.z-api.io/instances/${INSTANCIA_ID}/token/${TOKEN}/send-text`;
+const API_URL = 'https://api.z-api.io/instances/3E126FC63F55D002CB47AAEF140028B5/token/2041E2CA4AF17D4509230A8D/send-text';
+
 
 async function enviarResposta(telefone, mensagem) {
   try {
