86fb8349cc9ae78e2fe2952a608eca45e86670c9
Heliferreira <heliveltonsouza@outlook.com>
2025-05-14 22:12:04 -0300

    Corrige endpoint da Z-API para usar cabeçalho Client-Token

--- zapi.js
+++ zapi.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 
 const INSTANCIA_ID = '3E126FC63F55D002CB47AAEF140028B5';
 const TOKEN = '2041E2CA4AF17D4509230A8D';
+const API_URL = `https://api.z-api.io/instances/${INSTANCIA_ID}/send-text`;
 
-const API_URL = `https://api.z-api.io/instances/${INSTANCIA_ID}/token/${TOKEN}/send-text`;
 
 async function enviarResposta(telefone, mensagem) {
   try {
